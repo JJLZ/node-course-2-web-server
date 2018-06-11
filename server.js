41fb66b004db29f7982c015c1f959f75eefd70ec
Jose Lopez <jjlz.cocoa@gmail.com>
2018-06-11 13:46:23 -0500

    10. Adding a New Feature and Deploying

--- server.js
+++ server.js
@@ -65,6 +65,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio Page',
+    // currentYear: new Date().getFullYear()
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     error: 404,
